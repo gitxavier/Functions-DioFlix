2bb400232e7e2d9818bcc6ee7600e2e9c1431888
Gabriel  Xavier <gabriel.xavier@phsbrasil.com.br>
2025-02-04 09:39:45 -0300

    Primeiro commit - adicionando projeto FlixDIO

---
+++ fnPostDataStorage/fnPostDataStorage/Program.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var host = new HostBuilder()
+    .ConfigureFunctionsWebApplication()
+    .ConfigureServices(services =>
+    {
+        services.AddApplicationInsightsTelemetryWorkerService();
+        services.ConfigureFunctionsApplicationInsights();
+        services.Configure<KestrelServerOptions>(options =>
+        {
+            options.Limits.MaxRequestBodySize = 104857600; // 1024 * 1024 * 100; 
+        });
+    })
+    
+    .Build();
+
+host.Run();
---
+++ fnPostDatabase/Function1.cs
@@ -0,0 +1,40 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace fnPostDatabase
+{
+    public class Function1
+    {
+        private readonly ILogger<Function1> _logger;
+
+        public Function1(ILogger<Function1> logger)
+        {
+            _logger = logger;
+        }
+
+        [Function("movie")]
+        [CosmosDBOutput("%DatabaseName%", "%ContainerName%", Connection = "CosmosDBConnection", CreateIfNotExists =true, PartitionKey = "id")]
+        public async Task<Object?> Run([HttpTrigger(AuthorizationLevel.Function, "post")] HttpRequest req)
+        {
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
+
+            MovieRequest movie = null;
+            var content = await new StreamReader(req.Body).ReadToEndAsync();
+
+            try
+            {
+                movie = JsonConvert.DeserializeObject<MovieRequest>(content);
+            }
+            catch (Exception e)
+            {
+
+                return new BadRequestObjectResult("Erro ao deserializar o objeto: " + e.Message);
+            }
+
+            return JsonConvert.SerializeObject(movie);
+        }
+    }
+}
